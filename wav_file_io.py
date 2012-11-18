a57982b3ac8c2774b83ede98a6fa67e3f2781d8b
Benjamin Hitov <Benjamin.Hitov@gmail.com>
2012-11-18 03:09:04 -0500

    wave format

--- wav_file_io.py
+++ wav_file_io.py
@@ -1,13 +1,18 @@
 import optparse
 import os
+import time, datetime
 
 __author__ = 'ben'
 
 from twisted.internet import reactor, protocol
 
 from twisted.protocols import basic
+import wave
 
 from common import display_message #, validate_file_md5_hash, get_file_md5_hash, read_bytes_from_file, clean_and_split_input
+def dt2unix():
+    dt = datetime.datetime.now()
+    return str(time.mktime(dt.timetuple()) + (dt.microsecond / 100 ** 6))
 
 class WavReceiveProtocol(basic.LineReceiver):
     delimiter = '\n'
@@ -16,6 +21,7 @@ class WavReceiveProtocol(basic.LineReceiver):
         self.factory.clients.append(self)
         self.file_handler = None
         self.file_data = ()
+        self.file_contents = ''
 
         display_message('Connection from: %s (%d clients total)' % (self.transport.getPeer().host, len(self.factory.clients)))
 
@@ -27,6 +33,27 @@ class WavReceiveProtocol(basic.LineReceiver):
         #self.file_handler = None
         self.file_data = ()
 
+        f = wave.open('stuff/m_%s.wav' % dt2unix(), 'wb')
+        f.setnchannels(2)
+        f.setsampwidth(16)
+        f.setframerate(44100)
+
+        f.writeframes(self.file_contents)
+        f.close()
+
+        self.file_contents = ''
+
+
+#public class RecorderActivity extends Activity {
+#    private static final int RECORDER_BPP = 16;
+#private static final String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
+#private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+#private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
+#private static final int RECORDER_SAMPLERATE = 44100;
+#private static final int RECORDER_CHANNELS = 2;
+#private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+#        #
+
         display_message('Connection from %s lost (%d clients left)' % (self.transport.getPeer().host, len(self.factory.clients)))
 
     def lineReceived(self, line):
@@ -35,25 +62,27 @@ class WavReceiveProtocol(basic.LineReceiver):
         self.setRawMode()
 
     def rawDataReceived(self, data):
-        filename = self.file_data[0]
-        file_path = os.path.join(self.factory.files_path, filename)
+        #self.file_contents += data
+        #filename = self.file_data[0]
+        #file_path = os.path.join(self.factory.files_path, filename)
 
         display_message('Receiving file chunk (%d KB)' % (len(data)))
 
-        if not self.file_handler:
-            self.file_handler = open(file_path, 'wb')
+        #if not self.file_handler:
+        #    self.file_handler = open(file_path, 'wb')
 
         if data.endswith('\r\n'):
             # Last chunk
             data = data[:-2]
-            self.file_handler.write(data)
+            self.file_contents += data
             self.setLineMode()
 
-            self.file_handler.close()
-            self.file_handler = None
+            #self.file_handler.close()
+            #self.file_handler = None
 
         else:
-            self.file_handler.write(data)
+            self.file_contents += data
+            #self.file_handler.write(data)
 
     def _cleanAndSplitInput(self, input):
         input = input.strip()
